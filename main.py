f45bc634bd8d5190212ee9888505ad972567e65b
Patrick van Staveren <trick@vanstaveren.us>
2017-10-29 23:25:26 +0000

    use transmitter class, build basic sentence, configure gps, the works

--- main.py
+++ main.py
@@ -2,18 +2,25 @@
 
 import py_ublox_i2c
 import utils
+import transmitter
+
+callsign = "MCNAIR"
 
 if __name__ == "__main__":
+    # read state from disk, if this is mid flight?
     utils.enable_relay_uart_to_gps()
-    # configure gps over uart
+    py_ublox_i2c.configure_serial.configure()
     utils.disable_relay_uart_to_gps()
-    # configure uart for 50 baud
-    # enable mtx2 gpio pin
+    transmitter = transmitter.Transmitter()
+    transmitter.open_uart()
+    transmitter.enable_tx()
     # open i2c (?)
     while True:
         # save state
         # read bme280
-        # take photo
-        # read gps coords
-        # build sentence
-        # send sentence
+        # take photo - maybe in another process?
+        gps_location = py_ublox_i2c.read.read_gps()
+        sentence = [callsign]
+        sentence.append(gps_location.lat, gps_location.lon, alt)
+        sentence_string = sentence.join(',')
+        transmitter.send_sentence(sentence_string)
