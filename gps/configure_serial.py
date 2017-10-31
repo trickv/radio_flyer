6f2b1356dbe274e81e30b2c5b3be5f00092dbdbc
Patrick van Staveren <trick@vanstaveren.us>
2017-10-31 15:21:52 +0000

    py_ublox_i2c: add function to re-enable various default strings

--- gps/configure_serial.py
+++ gps/configure_serial.py
@@ -3,24 +3,37 @@
 import serial
 
 # FIXME: Enable flight mode
-# TODO: disable unnecessary strings by sending $PUBX strings
+# TODO: is it possible to read the configuration to verify
+# flight mode has actually been enabled?
 
-def configure():
-    gps_serial = serial.Serial('/dev/ttyAMA0', 9600, timeout=1)
+def _open_serial():
+    return serial.Serial('/dev/ttyAMA0', 9600, timeout=1)
+
+def configure_for_flight():
+    gps_serial = _open_serial()
     disable_excessive_reports(gps_serial)
     gps_serial.close()
 
 
+def configure_to_defaults():
+    gps_serial = _open_serial()
+    set_excessive_reports(gps_serial, enable=True)
+    gps_serial.close()
+
 def enable_flight_mode(gps_serial):
+    # FIXME UNTESTED!
     # following is from https://github.com/Chetic/Serenity/blob/master/Serenity.py#L10
     mode_string = bytearray.fromhex("B5 62 06 24 24 00 FF FF 06 03 00 00 00 00 10 27 00 00 05 00 FA 00 FA 00 64 00 2C 01 00 00 00 00 00 00 00 00 00 00 00 00 00 00 16 DC")
     for character in mode_string:
         gps_serial.write(chr(character))
     gps_serial.write("\r\n")
 
-
 def disable_excessive_reports(gps_serial):
-    disable_template = "PUBX,40,%s,0,0,0,0"
+    return set_excessive_reports(gps_serial, enable=False)
+
+
+def set_excessive_reports(gps_serial, enable = False):
+    disable_template = "PUBX,40,%s,%d,0,0,0"
     messages_disable = [
         "GLL",
         "GSA",
@@ -29,7 +42,7 @@ def disable_excessive_reports(gps_serial):
         "VTG",
     ]
     for message in messages_disable:
-        disable_command = disable_template % message
+        disable_command = disable_template % (message, 1 if enable else 0)
         checksum_int = 0
         for character in disable_command:
             checksum_int ^= ord(character)
@@ -46,4 +59,4 @@ def __send_thingymabob():
     gps_serial.close()
 
 if __name__ == "__main__":
-    configure()
+    configure_for_flight()
