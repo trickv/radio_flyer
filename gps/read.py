121b266f95601f0f605e27659ebf6a0efdca247a
Patrick van Staveren <trick@vanstaveren.us>
2017-11-04 23:34:24 +0000

    py_ublox_i2c: remove broken block read function; can't figure out but I think this never worked.

--- gps/read.py
+++ gps/read.py
@@ -52,29 +52,6 @@ def read_gps(i2c_address):
     msg = pynmea2.parse(response_chars, check=True)
     return(msg)
 
-def __read_gps_i2c_blockread(i2c_address):
-    """
-    This should perform better and worked in some of my tests, but seems to be throwing
-    a lot more I/O errors now. So use read_gps instead, which reads 1 byte at a time.
-    """
-    response_bytes = []
-    while True: # Newline, or bad char.
-        block = BUS.read_i2c_block_data(i2c_address, 0, 16)
-        last_byte = block[-1]
-        if last_byte == 255:
-            return False
-        elif last_byte > 126: # TODO: This (for me) is a symptom of i2c bus problems. Throw?
-            print("Unprintable char int={0}, chr={1}".format(last_byte, chr(last_byte)))
-            return False
-        elif last_byte == 10: # new line character
-            break
-        else:
-            response_bytes = response_bytes + block
-    response_chars = ''.join(chr(byte) for byte in response_bytes)
-    print("LINE: %s" % response_chars)
-    msg = pynmea2.parse(response_chars, check=True)
-    return(msg)
-
 def simple_read_demo():
     connect_bus()
     global DEBUG
