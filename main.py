f97c0dc01a31682a62ae158c48bfee3653899072
Patrick van Staveren <trick@vanstaveren.us>
2017-11-27 05:02:56 +0000

    fix checksum formatting to be left-zero padded

--- main.py
+++ main.py
@@ -20,16 +20,10 @@ BUS = None # FIXME I don't think this is needed here, it's a global from within 
 operational_packet_template = "{callsign},{seq},{time},{lat},{lon},{alt},{num_sats},{num_gps_reads},{temperature},{pressure},{humidity}"
 no_fix_packet_template = "{callsign},{seq},NOFIX,{time},{num_sats},{num_gps_reads},{temperature},{pressure},{humidity},{uptime}"
 # TODO: should I send \r\n or can we just all be unix friends from now on?
-# FIXME: BUG! need to left zero pad the checksum here
-# ex: $$RADIOFLYER,20,04:43:23,41.899755,-84.047956,236,9,4,18.91,977.68,42.29*73D
-# should be
-#     $$RADIOFLYER,20,04:43:23,41.899755,-84.047956,236,9,4,18.91,977.68,42.29*073D
-# also bad:
-#     $$RADIOFLYER,69,17:19:21,41.899707,-84.048145,239,6,2,24.46,964.91,36.65*27A
 # try: http://habitat.habhub.org/genpayload/
 #      payload -> create new
 #      new format wizard
-sentence_template = "$${0}*{1:X}\n"
+sentence_template = "$${0}*{1:04X}\n"
 
 
 def configure_ublox():
