40e03d77362c1695543a4d134eb2a7e145fe4ea6
Patrick van Staveren <trick@vanstaveren.us>
2018-01-23 07:47:14 +0000

    don't leak file handles for smbus

--- lm75/__init__.py
+++ lm75/__init__.py
@@ -6,13 +6,12 @@ class Lm75():
     By default the address of LM75 sensors are set to 0x48
     aka A0, A1, and A2 are set to GND (0v).
     """
-    def __init__(self, address=0x48, bus = 1):
+    def __init__(self, address=0x48, bus_id = 1):
         self.address = address
-        self.bus = bus
+        self.bus = smbus.SMBus(bus_id)
 
     def get_temperature(self):
         # Read I2C data and calculate temperature
-        bus = smbus.SMBus(self.bus)
         raw = bus.read_word_data(self.address, 0) & 0xFFFF
         raw = ((raw << 8) & 0xFF00) + (raw >> 8)
         temperature = (raw / 32.0) / 8.0
