746476d9b09ca13b97b0595e8a9c76586180cc76
Patrick van Staveren <trick@vanstaveren.us>
2018-01-23 01:00:46 +0000

    add some simple code for reading a lm75 sensor which I'll use for internal temperature

---
+++ Lm75/__init__.py
@@ -0,0 +1,19 @@
+import smbus
+
+class Lm75():
+
+    """
+    By default the address of LM75 sensors are set to 0x48
+    aka A0, A1, and A2 are set to GND (0v).
+    """
+    def __init__(self, address=0x48, bus = 1):
+        self.address = address
+        self.bus = bus
+
+    def get_temperature(self):
+        # Read I2C data and calculate temperature
+        bus = smbus.SMBus(self.bus)
+        raw = bus.read_word_data(self.address, 0) & 0xFFFF
+        raw = ((raw << 8) & 0xFF00) + (raw >> 8)
+        temperature = (raw / 32.0) / 8.0
+        return temperature
