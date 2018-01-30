9da71846f20e5d62d72a9d8ccd29e486975c4e43
Patrick van Staveren <trick@vanstaveren.us>
2018-01-30 13:39:07 +0000

    modify this to a static version

--- read-gps.py
+++ read-gps.py
@@ -3,14 +3,14 @@
 import lib
 import time
 
-
 g = lib.Gps()
 
 import random
 
 while True:
     print(repr(g.read()))
-    sleep_amount = random.randint(0,10000) / 1000.0
-    print("\nMAIN: sleeping {} seconds.".format(sleep_amount))
+    #sleep_amount = random.randint(0,10000) / 1000.0
+    sleep_amount = 5
+    #print("\nMAIN: sleeping {} seconds.".format(sleep_amount))
     time.sleep(sleep_amount)
-    print("MAIN: slept {} seconds.".format(sleep_amount))
+    #print("MAIN: slept {} seconds.".format(sleep_amount))
