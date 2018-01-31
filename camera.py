0ccc2eee750dc5f815ffee3cd2c757a901deb29b
Patrick van Staveren <trick@vanstaveren.us>
2018-01-31 20:54:42 +0000

    add docstrings to camera.py

--- camera.py
+++ camera.py
@@ -1,11 +1,14 @@
 #!/usr/bin/env python3
 
+""" replacement implementation for taking photos from the main tracker """
+
 import time
 
 import lib
 
 
 def main():
+    """ main loop, never exits """
     print("Camera capture startup")
     camera = lib.Camera()
     while True:
