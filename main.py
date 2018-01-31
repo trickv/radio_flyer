323ed6ba8c7377c8e4c23b6f1fa72cdede7dd085
Patrick van Staveren <trick@vanstaveren.us>
2018-01-31 01:07:23 +0000

    convert to new GPS read method over USB-to-serial. Not yet entirely removed message output disabling code.

--- main.py
+++ main.py
@@ -7,29 +7,21 @@ import crcmod
 from bme280 import bme280, bme280_i2c
 import lib
 
-import gps.read
-import gps.configure_serial
 import utils
 from conf import CONF as conf
 
-BUS = None # FIXME I don't think this is needed here, it's a global from within the ublox lib
-
 PACKET_TEMPLATES = {
     'operational': "{callsign},{seq},{time},{lat},{lon},{alt}," +
-        "{num_sats},{num_gps_reads},{temperature},{pressure},{humidity}," +
+        "{num_sats},{temperature},{pressure},{humidity}," +
         "{internal_temperature}",
     'no_fix': "{callsign},{seq},NOFIX,{time},{num_sats}," +
-        "{num_gps_reads},{temperature},{pressure},{humidity},{uptime}," +
+        "{temperature},{pressure},{humidity},{uptime}," +
         "{internal_temperature}",
 }
 # try: http://habitat.habhub.org/genpayload/
 #      payload -> create new
 #      new format wizard
 sentence_template = "$${0}*{1:04X}\r\n"
-
-
-def configure_ublox():
-    gps.configure_serial.configure_for_flight()
 
 
 def setup_bme280():
@@ -42,46 +34,27 @@ def main():
     sequence = 0
     had_initial_fix = False
     transmitter = lib.Transmitter()
-    try:
-        configure_ublox()
-    except Exception as e:
-        transmitter.send("Exception when opening GPS serial: {}\r\n\r\n".format(e))
-        raise
     rendered_conf = utils.render_conf(conf)
     print(rendered_conf)
     transmitter.send(rendered_conf)
     transmitter.send("Worlds best tracker software. Buy bitcoin!\r\n\r\n")
     transmitter.send("Thanks to my lovely wife Sarah.\r\n\r\n")
-    gps.read.connect_bus()
+    gps = lib.Gps()
     setup_bme280()
     lm75_sensor = lib.Lm75()
     crc16f = crcmod.predefined.mkCrcFun('crc-ccitt-false')
 
-    num_gps_reads = 0
     while True:
         gps_location = None
-        try:
-            num_gps_reads += 1
-            gps_location = gps.read.read_gps(conf['ublox_i2c_address'])
-        except gps.read.BadDataException:
-            utils.print_status_char("!")
-            time.sleep(0.5)
-            continue
-        except KeyError:
-            utils.print_status_char("K")
-        except IOError:
-            utils.print_status_char("I")
-        except pynmea2.nmea.ParseError:
-            utils.print_status_char("P")
+        gps_location = gps.read()
         if not gps_location:
             utils.print_status_char(".")
-            time.sleep(0.5)
+            time.sleep(2)
             continue
         bme280_data = bme280.read_all()
         packet_params = {
             'callsign': conf['callsign'],
             'seq': sequence,
-            'num_gps_reads': num_gps_reads,
             'temperature': round(bme280_data.temperature, 2),
             'humidity': round(bme280_data.humidity, 2),
             'pressure': round(bme280_data.pressure, 2),
@@ -108,6 +81,7 @@ def main():
                 })
         else:
             # Oh shit, the GPS is sending things I don't know how to handle
+            # FIXME remove this else, this is all unncessary code! :)
             crazy = "%s: Unexpected GPS data: %s\r\n" % (conf['callsign'], gps_location)
             transmitter.send(crazy)
             if gps_location.sentence_type in ("GLL", "GSA", "RMC", "GSV", "VTG"):
@@ -117,7 +91,7 @@ def main():
                 time.sleep(10)
                 transmitter.close_uart()
                 print("UART closed, go go go")
-                configure_ublox()
+                #configure_ublox()
                 transmitter.open_uart()
             continue
         packet = packet_template.format(**packet_params)
